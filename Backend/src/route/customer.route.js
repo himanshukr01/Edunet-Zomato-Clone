bc2af35cc2d5b0af9af4325f7c73e861f8d531ba
Himanshu kumar <himanshukr1505892@gmail.com>
2025-02-25 23:49:28 +0530

    first commit

--- Backend/src/route/customer.route.js
+++ Backend/src/route/customer.route.js
@@ -10,7 +10,7 @@ import {
 
 const router = Router();
 
-
+router.route("/register").post(registerCustomer);
 router.route("/login").post(loginCustomer);
 router.route("/updatecustomerdetails").put(updateCustomerDetails);
 router.route("/createorder").post(createOrder);
