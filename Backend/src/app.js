30a6395e8d5ac24a451710a59f4100288b7192ea
Himanshu kumar <himanshukr1505892@gmail.com>
2025-02-28 12:02:08 +0530

    second commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 
 const app = express();
 
+
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
--- frontend/src/routecomponent/signupPage/Signup.jsx
+++ frontend/src/routecomponent/signupPage/Signup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Signup.css"
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { sendEmailOtp } from '../../databaseCall/sendEmailOtp.js';
+// import { sendEmailOtp } from '../../databaseCall/sendEmailOtp.js';
 import { useRef } from 'react';
 import { customerRegistration } from '../../databaseCall/registerCustomer.js';
 import { useNavigate } from 'react-router-dom';
@@ -16,16 +16,15 @@ const Signup = () => {
 
     const [firstName, setFirstname] = useState('');
     const [lastName, setLastname] = useState('');
-    // const [useremail, setUseremail] = useState('');
-    // const [userpassword, setUserpassword] = useState('')
-    const [verifyOtp, setVerifyOtp] = useState(false)
-    const [inputOtp, setInputOtp] = useState(Array.from({ length: 6 }, () => ""));
-    const [otp, setOtp] = useState("")
-    const [currentFocus, setCurrentFocus] = useState(0)
+    const [useremail, setUseremail] = useState('');
+    const [userpassword, setUserpassword] = useState('')
+    // const [verifyOtp, setVerifyOtp] = useState(false)
+    // const [inputOtp, setInputOtp] = useState(Array.from({ length: 6 }, () => ""));
+    // const [otp, setOtp] = useState("")
+    // const [currentFocus, setCurrentFocus] = useState(0)
 
     const [signupSuccess, setSignupSuccess] = useState(false)
-    let inputRef = useRef(null)
-
+    // let inputRef = useRef(null)
 
     // const handleSubmit = async (e) => {
     //     e.preventDefault()
@@ -45,111 +44,111 @@ const Signup = () => {
    
     const handleSubmit = (e) => {
         e.preventDefault();
-    
         
         console.log('Email is', useremail);
         console.log('Password is', userpassword); 
+
+        setUseremail('')
+        setUserpassword('')
+
+        navigate('/login')
     };
     
-    const handleChange = (e, index) => {
+    // const handleChange = (e, index) => {
+    //     if (inputOtp[index].length < 1) {
+    //         const value = e.target.value;
+    //         console.log("val is: ", value);
+    //         setInputOtp(prevState => {
+    //             const newState = [...prevState];
+    //             newState[index] = value;
+    //             return newState;
+    //         });
+    //         if (index < 5) {
+    //             setCurrentFocus(index + 1)
+    //             inputRef.current.focus()
+    //         }
+    //     }
+    // }
 
-        if (inputOtp[index].length < 1) {
-            const value = e.target.value;
-            console.log("val is: ", value);
-            setInputOtp(prevState => {
-                const newState = [...prevState];
-                newState[index] = value;
-                return newState;
-            });
-            if (index < 5) {
-                setCurrentFocus(index + 1)
-                inputRef.current.focus()
-            }
-        }
-    }
+    // const handleFocus = (e, index) => {
+    //     console.log("e", e);
+    //     e.target.focus()
+    //     setCurrentFocus(index)
+    // }
 
-    const handleFocus = (e, index) => {
-        console.log("e", e);
-        e.target.focus()
-        setCurrentFocus(index)
-    }
+    // const handleKeyDown = (e, index) => {
+    //     console.log("e on key down: ", e);
+    //     if (e.key === 'Backspace') {
+    //         setInputOtp(prevState => {
+    //             const newState = [...prevState];
+    //             newState[index] = "";
+    //             return newState;
+    //         });
+    //     }
+    //     console.log(e);
+    // }
 
-    const handleKeyDown = (e, index) => {
-        console.log("e on key down: ", e);
-        if (e.key === 'Backspace') {
-            setInputOtp(prevState => {
-                const newState = [...prevState];
-                newState[index] = "";
-                return newState;
-            });
-        }
-        console.log(e);
+    // const handleVerifyEmail = async (e) => {
+    //     e.preventDefault()
 
-    }
+    //     if (inputOtp.some(digit => digit === '')) {
+    //         return;
+    //     }
 
-    const handleVerifyEmail = async (e) => {
-        e.preventDefault()
+    //     let inputOtpStr = ""
+    //     inputOtp.forEach((e) => {
+    //         inputOtpStr += e
+    //     })
 
-        if (inputOtp.some(digit => digit === '')) {
-            return;
-        }
+    //     if (inputOtpStr === String(otp)) {
+    //         customerRegistration(firstName, lastName, useremail, userpassword)
+    //             .then((res) => {
+    //                 console.log('RES rec is: ', res);
+    //                 dispatch(setCustomerDetail(res))
+    //                 navigate("/")
+    //             })
+    //             .catch(() => {
 
-        let inputOtpStr = ""
-        inputOtp.forEach((e) => {
-            inputOtpStr += e
-        })
+    //             })
+    //     }
+    // }
 
-        if (inputOtpStr === String(otp)) {
-            customerRegistration(firstName, lastName, useremail, userpassword)
-                .then((res) => {
-                    console.log('RES rec is: ', res);
-                    dispatch(setCustomerDetail(res))
-                    navigate("/")
-                })
-                .catch(() => {
-
-                })
-        }
-    }
-
-
-    console.log("inp otp : ", inputOtp);
+    // console.log("inp otp : ", inputOtp);
 
     return (
         <div className='Signup-parent'>
-            {
+            {/* {
                 verifyOtp ?
-                    // <div className='verifyOtp'>
-                    //     <div className='heading'>Verify Your email</div>
-                    //     <div className='para'>
-                    //         <div className='div1'>Enter the 8 digit code you have received on</div>
-                    //         <div className='div2'>{useremail.slice(0, 3)}***{useremail.slice(useremail.length - 10, useremail.length)}</div>
-                    //     </div>
+                    <div className='verifyOtp'>
+                        <div className='heading'>Verify Your email</div>
+                        <div className='para'>
+                            <div className='div1'>Enter the 8 digit code you have received on</div>
+                            <div className='div2'>{useremail.slice(0, 3)}***{useremail.slice(useremail.length - 10, useremail.length)}</div>
+                        </div>
 
-                    //     <form className='form' onSubmit={handleVerifyEmail}>
-                    //         <label htmlFor="">Code</label>
-                    //         <div className="form-child">
-                    //             {Array.from({ length: 6 }).map((_, i) => (
-                    //                 <input
-                    //                     onFocus={(e) => handleFocus(e, i)}
-                    //                     key={i}
-                    //                     ref={i === currentFocus + 1 ? inputRef : null}
-                    //                     onChange={(e) => handleChange(e, i)}
-                    //                     onKeyDown={(e) => handleKeyDown(e, i)}
-                    //                     className='input'
-                    //                     type="number"
-                    //                     maxLength={1}
-                    //                     value={inputOtp[i]}
-                    //                 />
+                        <form className='form' onSubmit={handleVerifyEmail}>
+                            <label htmlFor="">Code</label>
+                            <div className="form-child">
+                                {Array.from({ length: 6 }).map((_, i) => (
+                                    <input
+                                        onFocus={(e) => handleFocus(e, i)}
+                                        key={i}
+                                        ref={i === currentFocus + 1 ? inputRef : null}
+                                        onChange={(e) => handleChange(e, i)}
+                                        onKeyDown={(e) => handleKeyDown(e, i)}
+                                        className='input'
+                                        type="number"
+                                        maxLength={1}
+                                        value={inputOtp[i]}
+                                    />
 
-                    //             ))}
-                    //         </div>
-                    //         <button className='verify-button ' type="submit">Verify</button>
-                    //     </form>
+                                ))}
+                            </div>
+                            <button className='verify-button ' type="submit">Verify</button>
+                        </form>
 
-                    // </div>
-                    null
-                    :
+                    </div>
+                    : */}
                     <div className='Signup'>
                         <div className='heading'>Create your account</div>
 
@@ -186,7 +185,7 @@ const Signup = () => {
                             </Link>
                         </div>
                     </div>
-            }
+            {/* } */}
         </div>
     )
 }
